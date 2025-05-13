cefd9d3c1ac6c6a6c472c9c053ccef670de13d56
Vrushabh Veer <veervrushabh@gmail.com>
2025-05-13 22:54:52 +0530

    update 404

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,20 +1,29 @@
 import { Link } from "react-router-dom";
-import { IconHome } from "@tabler/icons-react";
+import { IconMoodConfuzed } from "@tabler/icons-react";
 
 function NotFound() {
   return (
-    <div className="container-custom py-20 flex flex-col items-center justify-center text-center">
-      <h1 className="text-5xl md:text-7xl font-bold text-gray-900 mb-4">404</h1>
-      <h2 className="text-2xl md:text-3xl font-bold text-gray-800 mb-6">
-        Page Not Found
-      </h2>
-      <p className="text-lg text-gray-600 max-w-md mb-8">
-        The page you&apos;re looking for doesn&lsquo;t exist or has been moved.
-      </p>
-      <Link to="/" className="btn btn-primary flex items-center gap-2">
-        <IconHome size={18} />
-        Go Home
-      </Link>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="flex flex-col items-center justify-center text-center">
+        <h1 className="text-9xl font-bold mb-2">
+          4<span className="text-orange-500">0</span>4
+        </h1>
+
+        <h2 className="text-2xl md:text-3xl font-bold text-gray-800 mb-4">
+          Page Not Found
+        </h2>
+        <p className="w-[90%] text-lg text-gray-600 max-w-md mb-8">
+          The page you&apos;re looking for doesn&lsquo;t exist or has been
+          moved.
+        </p>
+        <Link
+          to="/"
+          className="btn btn-primary flex items-center gap-1 bg-orange-50 p-2 rounded-lg text-orange-500 font-semibold border-b-2 border-orange-300"
+        >
+          <IconMoodConfuzed size={28} />
+          Go Home
+        </Link>
+      </div>
     </div>
   );
 }
