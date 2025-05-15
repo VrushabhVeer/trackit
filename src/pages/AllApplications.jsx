cab4ade6dd70ffaa767e735e66af149b21feefa6
Vrushabh Veer <veervrushabh@gmail.com>
2025-05-15 12:20:50 +0530

    update the search functionality

--- src/pages/AllApplications.jsx
+++ src/pages/AllApplications.jsx
@@ -79,7 +79,7 @@ const AllApplications = () => {
               <input
                 type="text"
                 className="block w-full pl-10 pr-3 py-2.5 border border-gray-300 rounded-md leading-5 bg-white placeholder-gray-500 sm:text-sm"
-                placeholder="Search by company, date & position aplied"
+                placeholder="Search by company, date & position applied"
                 value={searchTerm}
                 onChange={(e) => setSearchTerm(e.target.value)}
               />
